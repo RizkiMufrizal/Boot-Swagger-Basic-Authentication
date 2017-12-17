a206c5cf3d2e9450ce888bf8d1f8327ab997af2f
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-12-17 19:26:28 +0700

    add validation id helper

---
+++ src/main/kotlin/org/rizki/mufrizal/boot/swagger/helpers/ValidationIdHelper.kt
@@ -0,0 +1,18 @@
+package org.rizki.mufrizal.boot.swagger.helpers
+
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Web <https://RizkiMufrizal.github.io>
+ * @Since 17 December 2017
+ * @Time 3:52 PM
+ * @Project Boot-Swagger
+ * @Package org.rizki.mufrizal.boot.swagger.helpers
+ * @File ValidationIdHelper
+ *
+ */
+
+interface ValidationIdHelper {
+    fun validateSelf(id: String)
+}

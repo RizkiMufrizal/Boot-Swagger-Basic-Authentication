bb1f6091667eb4e5399d45f98128d331e95b5b4b
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-12-17 19:26:48 +0700

    add mapping message

---
+++ src/main/kotlin/org/rizki/mufrizal/boot/swagger/helpers/swaggermapping/MappingMessage.kt
@@ -0,0 +1,16 @@
+package org.rizki.mufrizal.boot.swagger.helpers.swaggermapping
+
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Web <https://RizkiMufrizal.github.io>
+ * @Since 17 December 2017
+ * @Time 6:34 PM
+ * @Project Boot-Swagger
+ * @Package org.rizki.mufrizal.boot.swagger.helpers.swaggermapping
+ * @File MappingMessage
+ *
+ */
+
+data class MappingMessage(val Success: Boolean? = null, val Message: String? = null)

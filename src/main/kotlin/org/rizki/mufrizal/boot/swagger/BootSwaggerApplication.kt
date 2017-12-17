08fc89764c77d61da1faf02f7e2ecc59131ccb48
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-12-17 19:28:26 +0700

    add main class

---
+++ src/main/kotlin/org/rizki/mufrizal/boot/swagger/BootSwaggerApplication.kt
@@ -0,0 +1,23 @@
+package org.rizki.mufrizal.boot.swagger
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Web <https://RizkiMufrizal.github.io>
+ * @Since 17 December 2017
+ * @Time 3:48 PM
+ * @Project Boot-Swagger
+ * @Package org.rizki.mufrizal.boot.swagger
+ * @File BootSwaggerApplication
+ *
+ */
+
+@SpringBootApplication
+class BootSwaggerApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(BootSwaggerApplication::class.java, *args)
+}

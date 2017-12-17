a4121f4250cb6e6825643a43d0131c458bad6c58
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-12-17 19:27:13 +0700

    add jenis barang

---
+++ src/main/kotlin/org/rizki/mufrizal/boot/swagger/domain/JenisBarang.kt
@@ -0,0 +1,18 @@
+package org.rizki.mufrizal.boot.swagger.domain
+
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Web <https://RizkiMufrizal.github.io>
+ * @Since 17 December 2017
+ * @Time 3:46 PM
+ * @Project Boot-Swagger
+ * @Package org.rizki.mufrizal.boot.swagger.domain
+ * @File JenisBarang
+ *
+ */
+
+enum class JenisBarang {
+    cair, gas, padat
+}

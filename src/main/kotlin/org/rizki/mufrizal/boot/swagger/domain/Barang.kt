0bd16d322b09634a11c071f79c80329acb97883b
RizkiMufrizal <mufrizalrizki@gmail.com>
2018-01-08 10:40:56 +0700

    add example swagger

--- src/main/kotlin/org/rizki/mufrizal/boot/swagger/domain/Barang.kt
+++ src/main/kotlin/org/rizki/mufrizal/boot/swagger/domain/Barang.kt
@@ -1,5 +1,6 @@
 package org.rizki.mufrizal.boot.swagger.domain
 
+import io.swagger.annotations.ApiModelProperty
 import org.hibernate.annotations.GenericGenerator
 import org.hibernate.validator.constraints.NotEmpty
 import javax.persistence.Column
@@ -30,13 +31,16 @@ data class Barang(
         @GeneratedValue(generator = "uuid2")
         @GenericGenerator(name = "uuid2", strategy = "uuid2")
         @Column(length = 36, name = "id_barang")
+        @ApiModelProperty(example = "001", required = true)
         val idBarang: String? = null,
         @NotEmpty
         @NotNull
         @Column(length = 50, name = "nama_barang")
+        @ApiModelProperty(example = "rinso", required = true)
         val namaBarang: String? = null,
         @NotNull
         @Enumerated(EnumType.STRING)
+        @ApiModelProperty(example = "gas", required = true)
         @Column(length = 36, name = "jenis_barang")
         val jenisBarang: JenisBarang? = null
 )
